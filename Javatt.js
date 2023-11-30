8cc5eb99604e84df52bb53cab2fe5a9ac0a85c0b
Verlustdeslebens <151806647+Verlustdeslebens@users.noreply.github.com>
2023-11-30 10:21:49 +0500

    Add files via upload

---
+++ Javatt.js
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cocknut.</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Poppins:wght@600&family=Quicksand:wght@600&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ephesis&family=Montserrat:wght@500&family=Quicksand:wght@600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="site2.css">
+
+</head>
+<body>
+<div class="all"> 
+    <div class="navbar" id="header">
+        <a href="" class="logo"><div class="box"></div></a>
+        <div class="navitem">
+            <div class="navigation">
+                <a href="#" class="navhome">Home</a>
+                <a href="#vniz" class="nav">About</a>
+                <a href="#vniz2" class="nav">Contacts</a>
+                <a href="#" class="nav" id="show">Sign Up</a>
+            </div>
+                <div class="navimg">
+                    <a href="" class="nav1"><img src="insta.png" alt=""></a>
+                    <a href="" class="nav1"><img src="diskodo.png" alt=""></a>
+                    <a href="" class="nav1"><img src="wasap.png" alt=""></a>
+                    <a href="" class="nav1"><img src="telega.png" alt=""></a>
+                </div>
+        </div>
+    </div>
+    
+    <div class="body">
+
+
+
+
+    </div>
+
+
+
+
+    <div id="registration">
+        <div class="regimg"></div>
+        <a href="#" class="nav1 close" id="close"><img src="free-icon-close-page-9067990.png" alt=""></a>
+        <p class="regtxt1">coconut.</p>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+
+
+
+        <form>
+        <label class="regtxt" for="Email">Email</label>
+        
+        <br>
+        <input type="email" id="Email" name="Email" required>
+        <br>
+        <br>
+        <br>
+          <label class="regtxt" for="username">Login</label>
+          
+          <br>
+          <input type="text" id="username" name="username" required>
+          <br>
+          <br>
+          <br>
+
+          <label class="regtxt" for="password">Password</label>
+          
+          <br>
+          <input type="password" id="password" name="password" required>
+          <br>
+          <input type="submit" value="Create account">
+        </form>
+        
+      </div>
+
+
+
+
+
+
+
+
+</div>
+<div class="page2" id="vniz">
+
+</div>
+<div class="page3" id="vniz2">
+    <div class="contackts">
+        <div class="vid">
+
+        </div>
+        <div class="con">     
+        <p class="txtcon">About us</p>
+        <p class="txtcon">Couching</p>
+        <p class="txtcon">Help</p>
+<div class="con2">
+        <p><a href="">The Company</a></p>
+        <p><a href="">Our Approach</a></p>
+        <p><a href="">Solutions</a></p>
+    </div>
+    </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+        
+        
+        
+            
+        
+    
+
+
+
+    <script src="site3.js"></script>
+</body>
+</html>
